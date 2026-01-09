c916cb655551e9fd7d68277eeeaf685eb4e830c9
fmagoge <fmagoge@gmail.com>
2026-01-10 01:53:47 +0200

    Turn 1 - main

--- src/click/__init__.py
+++ src/click/__init__.py
@@ -7,106 +7,200 @@ composable.
 
 from __future__ import annotations
 
-from .core import Argument as Argument
-from .core import Command as Command
-from .core import CommandCollection as CommandCollection
-from .core import Context as Context
-from .core import Group as Group
-from .core import Option as Option
-from .core import Parameter as Parameter
-from .decorators import argument as argument
-from .decorators import command as command
-from .decorators import confirmation_option as confirmation_option
-from .decorators import group as group
-from .decorators import help_option as help_option
-from .decorators import make_pass_decorator as make_pass_decorator
-from .decorators import option as option
-from .decorators import pass_context as pass_context
-from .decorators import pass_obj as pass_obj
-from .decorators import password_option as password_option
-from .decorators import version_option as version_option
-from .exceptions import Abort as Abort
-from .exceptions import BadArgumentUsage as BadArgumentUsage
-from .exceptions import BadOptionUsage as BadOptionUsage
-from .exceptions import BadParameter as BadParameter
-from .exceptions import ClickException as ClickException
-from .exceptions import FileError as FileError
-from .exceptions import MissingParameter as MissingParameter
-from .exceptions import NoSuchOption as NoSuchOption
-from .exceptions import UsageError as UsageError
-from .formatting import HelpFormatter as HelpFormatter
-from .formatting import wrap_text as wrap_text
-from .globals import get_current_context as get_current_context
-from .termui import clear as clear
-from .termui import confirm as confirm
-from .termui import echo_via_pager as echo_via_pager
-from .termui import edit as edit
-from .termui import getchar as getchar
-from .termui import launch as launch
-from .termui import pause as pause
-from .termui import progressbar as progressbar
-from .termui import prompt as prompt
-from .termui import secho as secho
-from .termui import style as style
-from .termui import unstyle as unstyle
-from .types import BOOL as BOOL
-from .types import Choice as Choice
-from .types import DateTime as DateTime
-from .types import File as File
-from .types import FLOAT as FLOAT
-from .types import FloatRange as FloatRange
-from .types import INT as INT
-from .types import IntRange as IntRange
-from .types import ParamType as ParamType
-from .types import Path as Path
-from .types import STRING as STRING
-from .types import Tuple as Tuple
-from .types import UNPROCESSED as UNPROCESSED
-from .types import UUID as UUID
-from .utils import echo as echo
-from .utils import format_filename as format_filename
-from .utils import get_app_dir as get_app_dir
-from .utils import get_binary_stream as get_binary_stream
-from .utils import get_text_stream as get_text_stream
-from .utils import open_file as open_file
+# Lazy import mappings: attribute name -> (module, name in module)
+_lazy_imports: dict[str, tuple[str, str]] = {
+    # core
+    "Argument": (".core", "Argument"),
+    "Command": (".core", "Command"),
+    "CommandCollection": (".core", "CommandCollection"),
+    "Context": (".core", "Context"),
+    "Group": (".core", "Group"),
+    "Option": (".core", "Option"),
+    "Parameter": (".core", "Parameter"),
+    # decorators
+    "argument": (".decorators", "argument"),
+    "command": (".decorators", "command"),
+    "confirmation_option": (".decorators", "confirmation_option"),
+    "group": (".decorators", "group"),
+    "help_option": (".decorators", "help_option"),
+    "make_pass_decorator": (".decorators", "make_pass_decorator"),
+    "option": (".decorators", "option"),
+    "pass_context": (".decorators", "pass_context"),
+    "pass_obj": (".decorators", "pass_obj"),
+    "password_option": (".decorators", "password_option"),
+    "version_option": (".decorators", "version_option"),
+    # exceptions
+    "Abort": (".exceptions", "Abort"),
+    "BadArgumentUsage": (".exceptions", "BadArgumentUsage"),
+    "BadOptionUsage": (".exceptions", "BadOptionUsage"),
+    "BadParameter": (".exceptions", "BadParameter"),
+    "ClickException": (".exceptions", "ClickException"),
+    "FileError": (".exceptions", "FileError"),
+    "MissingParameter": (".exceptions", "MissingParameter"),
+    "NoSuchOption": (".exceptions", "NoSuchOption"),
+    "UsageError": (".exceptions", "UsageError"),
+    # formatting
+    "HelpFormatter": (".formatting", "HelpFormatter"),
+    "wrap_text": (".formatting", "wrap_text"),
+    # globals
+    "get_current_context": (".globals", "get_current_context"),
+    # termui
+    "clear": (".termui", "clear"),
+    "confirm": (".termui", "confirm"),
+    "echo_via_pager": (".termui", "echo_via_pager"),
+    "edit": (".termui", "edit"),
+    "getchar": (".termui", "getchar"),
+    "launch": (".termui", "launch"),
+    "pause": (".termui", "pause"),
+    "progressbar": (".termui", "progressbar"),
+    "prompt": (".termui", "prompt"),
+    "secho": (".termui", "secho"),
+    "style": (".termui", "style"),
+    "unstyle": (".termui", "unstyle"),
+    # types
+    "BOOL": (".types", "BOOL"),
+    "Choice": (".types", "Choice"),
+    "DateTime": (".types", "DateTime"),
+    "File": (".types", "File"),
+    "FLOAT": (".types", "FLOAT"),
+    "FloatRange": (".types", "FloatRange"),
+    "INT": (".types", "INT"),
+    "IntRange": (".types", "IntRange"),
+    "ParamType": (".types", "ParamType"),
+    "Path": (".types", "Path"),
+    "STRING": (".types", "STRING"),
+    "Tuple": (".types", "Tuple"),
+    "UNPROCESSED": (".types", "UNPROCESSED"),
+    "UUID": (".types", "UUID"),
+    # utils
+    "echo": (".utils", "echo"),
+    "format_filename": (".utils", "format_filename"),
+    "get_app_dir": (".utils", "get_app_dir"),
+    "get_binary_stream": (".utils", "get_binary_stream"),
+    "get_text_stream": (".utils", "get_text_stream"),
+    "open_file": (".utils", "open_file"),
+}
+
+# Deprecated imports with their replacements
+_deprecated_imports: dict[str, tuple[str, str, str]] = {
+    # name -> (module, actual_name, warning_message)
+    "BaseCommand": (
+        ".core",
+        "_BaseCommand",
+        "'BaseCommand' is deprecated and will be removed in Click 9.0. Use"
+        " 'Command' instead.",
+    ),
+    "MultiCommand": (
+        ".core",
+        "_MultiCommand",
+        "'MultiCommand' is deprecated and will be removed in Click 9.0. Use"
+        " 'Group' instead.",
+    ),
+    "OptionParser": (
+        ".parser",
+        "_OptionParser",
+        "'OptionParser' is deprecated and will be removed in Click 9.0. The"
+        " old parser is available in 'optparse'.",
+    ),
+}
+
+__all__ = [
+    # core
+    "Argument",
+    "Command",
+    "CommandCollection",
+    "Context",
+    "Group",
+    "Option",
+    "Parameter",
+    # decorators
+    "argument",
+    "command",
+    "confirmation_option",
+    "group",
+    "help_option",
+    "make_pass_decorator",
+    "option",
+    "pass_context",
+    "pass_obj",
+    "password_option",
+    "version_option",
+    # exceptions
+    "Abort",
+    "BadArgumentUsage",
+    "BadOptionUsage",
+    "BadParameter",
+    "ClickException",
+    "FileError",
+    "MissingParameter",
+    "NoSuchOption",
+    "UsageError",
+    # formatting
+    "HelpFormatter",
+    "wrap_text",
+    # globals
+    "get_current_context",
+    # termui
+    "clear",
+    "confirm",
+    "echo_via_pager",
+    "edit",
+    "getchar",
+    "launch",
+    "pause",
+    "progressbar",
+    "prompt",
+    "secho",
+    "style",
+    "unstyle",
+    # types
+    "BOOL",
+    "Choice",
+    "DateTime",
+    "File",
+    "FLOAT",
+    "FloatRange",
+    "INT",
+    "IntRange",
+    "ParamType",
+    "Path",
+    "STRING",
+    "Tuple",
+    "UNPROCESSED",
+    "UUID",
+    # utils
+    "echo",
+    "format_filename",
+    "get_app_dir",
+    "get_binary_stream",
+    "get_text_stream",
+    "open_file",
+]
 
 
 def __getattr__(name: str) -> object:
-    import warnings
+    # Handle lazy imports
+    if name in _lazy_imports:
+        module_name, attr_name = _lazy_imports[name]
+        import importlib
 
-    if name == "BaseCommand":
-        from .core import _BaseCommand
+        module = importlib.import_module(module_name, __package__)
+        value = getattr(module, attr_name)
+        # Cache the import in the module's globals for subsequent access
+        globals()[name] = value
+        return value
 
-        warnings.warn(
-            "'BaseCommand' is deprecated and will be removed in Click 9.0. Use"
-            " 'Command' instead.",
-            DeprecationWarning,
-            stacklevel=2,
-        )
-        return _BaseCommand
+    # Handle deprecated imports
+    if name in _deprecated_imports:
+        import importlib
+        import warnings
 
-    if name == "MultiCommand":
-        from .core import _MultiCommand
+        module_name, attr_name, warning_msg = _deprecated_imports[name]
+        warnings.warn(warning_msg, DeprecationWarning, stacklevel=2)
+        module = importlib.import_module(module_name, __package__)
+        return getattr(module, attr_name)
 
-        warnings.warn(
-            "'MultiCommand' is deprecated and will be removed in Click 9.0. Use"
-            " 'Group' instead.",
-            DeprecationWarning,
-            stacklevel=2,
-        )
-        return _MultiCommand
-
-    if name == "OptionParser":
-        from .parser import _OptionParser
-
-        warnings.warn(
-            "'OptionParser' is deprecated and will be removed in Click 9.0. The"
-            " old parser is available in 'optparse'.",
-            DeprecationWarning,
-            stacklevel=2,
-        )
-        return _OptionParser
-
+    # Handle __version__ specially
     if name == "__version__":
         import importlib.metadata
         import warnings
